abce0b0d37d866c96993a5e499ee72cce8702ad6
Hrithik2212 <hr6466@srmist.edu.in>
2022-09-29 21:37:41 +0530

    first commit

---
+++ HelloWorld.js
@@ -0,0 +1,51 @@
+console.log("Hello World");
+
+var number =5;// in-line comment 
+console.log(number);
+
+/* Multi 
+line comment 
+*/
+
+number = 9 ; 
+
+// Data types and variables 
+
+/* Dara types of javascript 
+-> undefined
+-> null 
+-> boolean
+-> string 
+-> symbol 
+-> number
+-> object 
+*/
+
+// Variable 
+/* var - Global scope 
+let - local scope
+const - Non changable variable - constant */
+
+var myName = "Hrithik";
+console.log("My name is ",myName);
+myName = 8; 
+
+// Storing variables using assignment variable 
+
+var a ; 
+console.log(a)
+var b = 2; 
+a = 7 ; 
+a--;
+--a;
+console.log(a)
+b = a ; 
+b++;
+++b;
+console.log(b);
+
+// Float 
+var decimal = 5.7;
+console.log(decimal);
+var product = 2 * 2.5;
+console.log(product);
